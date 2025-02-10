96543a03d2abe975e4319bc0215e871f4328f2ab
Ahmed Matem <ahmedmatem@gmail.com>
2025-02-10 20:04:35 +0200

    Add ApplicationDbContext

---
+++ DocSpot.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿namespace DocSpot.Infrastructure.Data
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            this.ApplyAuditInfoRules();
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ DocSpot.Infrastructure/Data/Extensions.cs
@@ -0,0 +1,38 @@
+﻿namespace DocSpot.Infrastructure.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using DocSpot.Infrastructure.Data.Abstracts;
+
+    public static class Extensions
+    {
+        public static void ApplyAuditInfoRules(this ApplicationDbContext context)
+        {
+            var auditInfoEntries = context.ChangeTracker.Entries()
+                .Where(e => e.Entity is IAuditable
+                && (e.State == EntityState.Modified || e.State == EntityState.Added))
+                .ToList();
+
+            foreach (var entry in auditInfoEntries)
+            {
+                var entity = (BaseModel)entry.Entity;
+                if (entry.State == EntityState.Added)
+                {
+                    entity.CreatedOn = DateTime.Now;
+                }
+                else
+                {
+                    var dateTimeNow = DateTime.Now;
+                    entity.LastModifiedOn = dateTimeNow;
+                    if (entry.Entity is IDeletable)
+                    {
+                        if (((IDeletable)entry.Entity).IsDeleted)
+                        {
+                            entity.DeletedOn = dateTimeNow;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
