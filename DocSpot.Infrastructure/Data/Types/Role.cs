5763b52aaac9dda4ec3102b1651feb0520b2282a
Ahmed Matem <ahmedmatem@gmail.com>
2025-02-16 21:10:20 +0200

    Add type for app roles

---
+++ DocSpot.Infrastructure/Data/Types/Role.cs
@@ -0,0 +1,14 @@
+﻿#nullable disable
+namespace DocSpot.Infrastructure.Data.Types
+{
+    public class Role
+    {
+        public string Value { get; private set; }
+
+        private Role(string value) { Value = value; }
+
+        public static Role Admin => new Role("Admin");
+        public static Role Doctor => new Role("Doctor");
+        public static Role Patient => new Role("Patient");
+    }
+}
